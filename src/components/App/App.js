811847dae97d281eb0990a6ec5cfdd2fb59ff291
Amira Basyouni <basyouniamira1@gmail.com>
2025-06-25 16:02:38 -0700

    Project Complete! I'm working on a side project that requires documentation, so I added some notes to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,15 @@
+/* @purpose
+ * ToastPlayground is a temporary UI
+ * which will be used to create and test our toast component
+ * */
+
+/* @depth
+ * What is a toast component?
+ * a toast component is a UI element
+ * that displays brief, non-critical messages to users,
+ * typically in the form of a small popup or notification
+ * */
+
 import React from 'react';
 
 import ToastProvider from '../ToastProvider';
